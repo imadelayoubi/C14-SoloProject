3cea0584c2b5cd0f8558ab092b2e2ed57b7844ed
ImadElAyoubi <imadelayoubi07@gmail.com>
2021-05-24 16:17:15 +0100

    calculator structure

---
+++ calc.js
@@ -0,0 +1,8 @@
+// var calcbtn = [0]
+// for (var i = 0; i < 10; i++) {
+//     $('#calculator').append('<button class="calcnumbers"> ' + i + '</button>')
+// }
+// var symb = ['+', '-', '*', '/', '=']
+// for (var i = 0; i < symb.length; i++) {
+//     $('#calculator').prepend('<button class="calcsymb"> ' + symb[i] + '</button>')
+// }
